b7c560837895570efbe6eba6e2172ba5deac1a5c
Gabriel CROSMAN (94486) <gabriel.crosman@stud.acs.upb.ro>
2021-08-04 18:30:42 +0300

    Adaugare cookie

--- shopify/src/main/java/com/db/shopify/auth/AuthController.java
+++ shopify/src/main/java/com/db/shopify/auth/AuthController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,11 +20,12 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity authCustomer(@RequestBody LoginForm loginForm) {
+    public ResponseEntity authCustomer(@RequestBody LoginForm loginForm, HttpServletResponse response) {
         Optional<Customer> customerOp = authService.authCustomer(loginForm);
         if (customerOp.isPresent()) {
             String uniqueCode = UUID.randomUUID().toString();
             authService.identityMap.put(customerOp.get(), uniqueCode);
+            response.addCookie(new Cookie("uniqueCode", uniqueCode));
             return new ResponseEntity(HttpStatus.ACCEPTED);
         } else {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
